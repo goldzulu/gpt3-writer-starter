70ae68ad4388127ad5e804ca632852a71632176c
goldzulu <goldzulu222@gmail.com>
2022-12-29 04:18:42 +0000

    Added OG Images

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="InterviewBot Toolbox" key="title"/>
+        <meta property="og:description" content="powered by InterviewBot GPT3 AI" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://d3q6mrlpga8k3b.cloudfront.net/e7581c22-2dce-4b41-bd15-ed0a235d5425/meta.png"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
