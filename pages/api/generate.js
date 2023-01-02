abcae73580a3278ae30d1c241cac28d9f0bb4241
goldzulu <goldzulu222@gmail.com>
2023-01-02 02:21:36 +0000

    Improved prompts

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,76 +6,56 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix = 
-`
-Give me 5 interview questions and respective answers for the following role:
+const basePromptPrefix = `
+Give me 5 interview questions asked during a job interview for the following position. Where possible ask questions that are specific to the position. If an organisation is given, include questions specific to the organisation.
 
+Position:        
 `;
+
 const generateAction = async (req, res) => {
   // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
 
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
-    temperature: 0.7,
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.8,
     max_tokens: 500,
   });
   
-  const basePromptOutput = baseCompletion.data.choices.pop();
+  const basePromptOutput = await baseCompletion.data.choices.pop();
 
-  res.status(200).json({ output: basePromptOutput });
+  console.log(basePromptOutput.text);
+
+    // Add your second prompt here
+    const secondPrompt = `
+    Take the interview questions and the position of the job below and generate answers for each questions. Go deep into each one. Include the questions for each answers.
+
+    Position: ${req.body.userInput}
+
+    Interview Questions: ${basePromptOutput.text}
+
+    Interview Qestions with the respective answers:
+    `;
+
+    console.log(secondPrompt);
+    
+    // I call the OpenAI API a second time with Prompt #2
+    const secondPromptCompletion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${secondPrompt}`,
+      // I set a higher temperature for this one. Up to you!
+      temperature: 0.85,
+      // I also increase max_tokens.
+      max_tokens: 1250,
+    });
+    
+    // Get the output
+    const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+    // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+    res.status(200).json({ output: secondPromptOutput });
 };
 
-// POSSIBLE CHAINING EXAMPLE
-//
-// const basePromptPrefix =
-// `
-// Write me a detailed table of contents for a blog post with the title below.
-
-// Title:
-// `
-
-// const generateAction = async (req, res) => {
-//   console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
-
-//   const baseCompletion = await openai.createCompletion({
-//     model: 'text-davinci-003',
-//     prompt: `${basePromptPrefix}${req.body.userInput}`,
-//     temperature: 0.8,
-//     max_tokens: 250,
-//   });
-  
-//   const basePromptOutput = baseCompletion.data.choices.pop();
-
-//   // I build Prompt #2.
-//   const secondPrompt = 
-//   `
-//   Take the table of contents and title of the blog post below and generate a blog post written in thwe style of Paul Graham. Make it feel like a story. Don't just list the points. Go deep into each one. Explain why.
-
-//   Title: ${req.body.userInput}
-
-//   Table of Contents: ${basePromptOutput.text}
-
-//   Blog Post:
-//   `
-  
-//   // I call the OpenAI API a second time with Prompt #2
-//   const secondPromptCompletion = await openai.createCompletion({
-//     model: 'text-davinci-003',
-//     prompt: `${secondPrompt}`,
-//     // I set a higher temperature for this one. Up to you!
-//     temperature: 0.85,
-// 		// I also increase max_tokens.
-//     max_tokens: 1250,
-//   });
-  
-//   // Get the output
-//   const secondPromptOutput = secondPromptCompletion.data.choices.pop();
-
-//   // Send over the Prompt #2's output to our UI instead of Prompt #1's.
-//   res.status(200).json({ output: secondPromptOutput });
-// };
-// END POSSIBLE CHAINING EXAMPLE
 
 export default generateAction;
